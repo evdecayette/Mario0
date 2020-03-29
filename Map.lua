a75e1f9f7bfd23e434703dbe2205eed7fa4feb95
Decayette Evens <decayette.evens@gmail.com>
2020-03-29 16:39:21 -0700

    Setup and create class

---
+++ Map.lua
@@ -0,0 +1,17 @@
+Map = Class{}
+
+function Map:init()
+    self.spritesheet = love.graphics.newImage('graphics/spritesheet.png')
+    self.titleWidth = 16
+    self.titleHeight = 16
+    self.mapWidth = 30
+    
+end
+
+function Map:update(dt)
+
+end
+
+function Map:render()
+
+end 
